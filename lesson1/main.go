b2faea53940b0a1e19ebba0feeb6e5e7cb7d5e3f
enes <enesooo78@gmail.com>
2023-03-11 23:43:56 +0300

    dec

--- lesson1/main.go
+++ lesson1/main.go
@@ -2,7 +2,29 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("hello world")
-	
+	var name string ="enes"
+	var name2 string
+	var age int
+	age =22
+	name2="göktaş"
+	fmt.Println("hello ", name,name2, "yaşı:",age)
+
+
+	// var kullanımı
+	var(
+		name3="en"
+		age2 int =25
+		height float32=1.75
+	)
+	fmt.Println(name3,age2,height)
+
+	// tek satır var
+
+	var names1,ages1,marrieds1 = "enes", 25,true
+	fmt.Println(names1,ages1,marrieds1)
+
+	// tek satırda değer atama := ile
+	names2,ages2,marrieds2 := "enes", 25,true
+	fmt.Println(names2,ages2,marrieds2,":= kullanarak")
 
 }
